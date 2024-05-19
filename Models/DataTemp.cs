6406c6d690f84aceb0bf27bc26f2e0c0be2524f2
Şeyma <141773338+ozdemirseyma1363@users.noreply.github.com>
2024-05-19 13:02:46 +0300

    Add files via upload

---
+++ Models/DataTemp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaturaWeb.Models
+{
+    public class DataTemp
+    {
+        public static List<FaturaDetay> TempFaturaDetay { get; set; }
+    }
+}
---
+++ Models/FaturaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaturaWeb.Models
+{
+
+    public class PostObject//ileti
+    {
+        public int No { get; set; }
+        public string Find { get; set; }
+
+    }
+    public class ResponseObject//yanıt
+    {
+        public bool Result { get; set; }
+        public string Message { get; set; }
+    }
+}
